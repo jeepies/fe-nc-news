fe060c1e344032bfb5cffdc1583c899c0cae7475
jay <davisjayalan@gmail.com>
2024-11-04 13:20:02 +0000

    added basic API requests

---
+++ utils/api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://be-nc-news-47u0.onrender.com/api",
+  timeout: 1000,
+});
+
+/**
+ * @param {axios.AxiosResponse<any, any>} response
+ */
+const _validateResponseAndGrabData = (response) => {
+  if (response.status !== 200)
+    return Promise.reject(
+      `invalid status code. expected 200, got ${response.status}`
+    );
+  return response.data;
+};
+
+export const fetchTopics = () => {
+  return API.get("/topics").then((response) =>
+    _validateResponseAndGrabData(response)
+  );
+};
+
+export const fetchArticles = () => {
+  return API.get("/articles").then(
+    (response) => _validateResponseAndGrabData(response).articles
+  );
+};
