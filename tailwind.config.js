69a6712e80853f24bc685d0f0fafb6c127edd8f9
jay <davisjayalan@gmail.com>
2024-11-04 11:32:01 +0000

    added colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./src/**/*.{js,jsx}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "heavy-metal": "#252525",
+        "dark-grey": "#353535",
+        "blue-lotus": "#6C5CE7",
+        "iris": "#6256B9",
+        "success": "#55EFC4",
+        "fail": "#FF7675",
+      }
+    },
   },
   plugins: [],
 };
