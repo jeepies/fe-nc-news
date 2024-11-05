404b8c7fd02c0e9b211b6c96ca78b7127c97bf80
jay <davisjayalan@gmail.com>
2024-11-05 11:10:21 +0000

    added usercontext

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./contexts/User.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <UserProvider value={{ username: "test" }}>
+      <App />
+    </UserProvider>
   </BrowserRouter>
 );
