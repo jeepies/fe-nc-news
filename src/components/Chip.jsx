e9c240bf4d1b8bb6ca49e2a7f3face46a0165b13
jay <davisjayalan@gmail.com>
2024-11-05 12:10:19 +0000

    added ability to sort by topics

--- src/components/Chip.jsx
+++ src/components/Chip.jsx
@@ -1,4 +1,4 @@
 export default function Chip(props) {
-  const { text, useDark } = props;
-  return <span className={`${useDark ? "bg-heavy-metal" : "bg-dark-grey"} rounded-lg p-1 capitalize text-xs`}>{text}</span>;
+  const { text, useDark, onClick, className } = props;
+  return <span onClick={onClick} className={`${useDark ? "bg-heavy-metal" : "bg-dark-grey"} text-center text-white  rounded-lg p-1 capitalize text-xs hover:cursor-pointer ${className}`}>{text}</span>;
 }
--- src/routes/Articles.jsx
+++ src/routes/Articles.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { fetchArticles } from "../../utils/api";
+import { fetchArticles, fetchTopics } from "../../utils/api";
 import Wrapper from "../components/Wrapper";
 import ArticleCard from "../components/ArticleCard";
 import Chip from "../components/Chip"
@@ -8,6 +8,7 @@ import Chip from "../components/Chip"
 export default function Articles() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [articles, setArticles] = useState([]);
+  const [topics, setTopics] = useState([]);
 
   const possibleParameters = ["topic", "sort_by", "order"]
   const params = possibleParameters.filter((possibleParameter) => searchParams.has(possibleParameter))
@@ -15,17 +16,25 @@ export default function Articles() {
   params.forEach((param) => builtParams[param] = searchParams.get(param))
 
   useEffect(() => {
-    const request = fetchArticles(builtParams);
-    request.then((response) => {
-      setArticles(response);
-    });
-  }, []);
+    fetchArticles(builtParams).then((response) => setArticles(response));
+    fetchTopics().then((topics) => setTopics(topics))
+  }, [searchParams]);
+
+  const setTopic = (topic) => {
+    builtParams["topic"] = topic;
+    setSearchParams(builtParams);
+  }
 
   return (
-    <div className="m-1">
-      <Wrapper title="Articles">
-        {articles.map((article) => <ArticleCard article={article} />)}
-      </Wrapper>
+    <div className="m-1 space-y-1">
+      <div className="grid grid-cols-3 sm:grid-cols-12 gap-1">
+        {topics.map((topic) => <Chip onClick={() => setTopic(topic.slug)} className={builtParams["topic"] === topic.slug ? "border border-iris" : ""} useDark text={topic.slug} />)}
+      </div>
+      <div>
+        <Wrapper title="Articles">
+          {articles.map((article) => <ArticleCard article={article} />)}
+        </Wrapper>
+      </div>
     </div>
   );
 }
